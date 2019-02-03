4d5d45df330909cbcc6310a97cf8f5e84193697d
Vincent <vmarc29@hotmail.com>
2019-02-03 17:47:31 +0100

    Fix video issue

--- server/web/routes/video.route.js
+++ server/web/routes/video.route.js
@@ -7,8 +7,8 @@ const Auth = require('../../core/auth.core');
 const VideoController = require('../controller/video.controller');
 
 // Définition des routes
+router.post('/', Auth.isConnected, Auth.isAdmin, VideoController.createVideo);
 router.post('/:videoID', Auth.isConnected, Auth.isAdmin, VideoController.updateVideo);
-router.post('/:videoID', VideoController.updateVideo);
 
 router.get('/:videoID', Auth.isConnected, VideoController.getVideo);
 router.get('/', Auth.isConnected, VideoController.getAllVideos);
